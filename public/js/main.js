c92f420722d0b2f2fbf03621f18b4c14a07905d9
Julian Meyer <julianmeyer2000@gmail.com>
2015-11-08 01:14:27 -0800

    Added networks list

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,13 @@
 $(document).ready(function() {
-
-  // Place JavaScript code here...
-
+  if(document.location.pathname == "/networks") {
+    $.get("/networks/json", function(networks) {
+      networks.forEach(function(network) {
+        $("#networks").append('<tr><td>' + network.name + '</td><td>' + network.apiKey + '</td><td><button type="button" class="btn btn-danger" id="delete-network" data-delete-id="' + network._id + '">Delete</button></td></tr>')
+      })
+      $("#delete-network").click(function() {
+        $.post("/networks/delete", {networkID: $(this).data("delete-id")});
+        $(this).parent().parent().remove();
+      });
+    });
+  }
 });
