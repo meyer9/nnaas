250931c515771b78213b908c26628f3a9a16e557
Gefen Kohavi <gefenkohavi@Gefens-MacBook-Pro.local>
2015-11-07 16:19:22 -0800

    Added a new Function

--- NeuralNet.js
+++ NeuralNet.js
@@ -1,6 +1,44 @@
 Synaptic = require("synaptic");
+function Perceptron(input, hidden, output)
+{
+    // create the layers
+    var inputLayer = new Synaptic.Layer(input);
+    var hiddenLayer = new Synaptic.Layer(hidden);
+    var outputLayer = new Synaptic.Layer(output);
 
-var perceptron = new Synaptic.Architect.Perceptron(2,3,1);
-console.log(perceptron.trainer.XOR()); 
+    // connect the layers
+    inputLayer.project(hiddenLayer);
+    hiddenLayer.project(outputLayer);
 
-console.log(Math.floor(parseFloat(perceptron.activate([1,0]))+0.5));
+    // set the layers
+    this.set({
+        input: inputLayer,
+        hidden: [hiddenLayer],
+        output: outputLayer
+    });
+}
+Perceptron.prototype = new Synaptic.Network();
+Perceptron.prototype.constructor = Perceptron;
+
+
+
+var myPerceptron = new Synaptic.Perceptron(2,3,1);
+var myTrainer = new Trainer(myPerceptron);
+
+myTrainer.XOR(); // { error: 0.004998819355993572, iterations: 21871, time: 356 }
+
+console.log(myPerceptron.activate([0,0])); // 0.0268581547421616
+myPerceptron.activate([1,0]); // 0.9829673642853368
+myPerceptron.activate([0,1]); // 0.9831714267395621
+myPerceptron.activate([1,1]); // 0.02128894618097928
+
+function network()
+{
+	while()
+	{
+	
+	
+	}
+
+
+}
