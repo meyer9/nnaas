0768d29dea944925291ac3eea3559842eefdbe65
Gefen Kohavi <gefenkohavi@Gefens-MacBook-Pro.local>
2015-11-07 18:46:48 -0800

    Almost finished with multiple layer networks

--- NeuralNet.js
+++ NeuralNet.js
@@ -1,44 +1,60 @@
-Synaptic = require("synaptic");
-function Perceptron(input, hidden, output)
+var Synaptic = require("synaptic");
+function createNetwork(input, hidden, output, numHidden)
 {
-    // create the layers
-    var inputLayer = new Synaptic.Layer(input);
-    var hiddenLayer = new Synaptic.Layer(hidden);
+	// create the layers
+	var hiddenLayers = [];
+	var inputLayer = new Synaptic.Layer(input);
+	for(var index = 0; index < numHidden; index++){
+		hiddenLayers.push(new Synaptic.Layer(hidden));
+	}
     var outputLayer = new Synaptic.Layer(output);
 
     // connect the layers
-    inputLayer.project(hiddenLayer);
-    hiddenLayer.project(outputLayer);
-
+    inputLayer.project(hiddenLayers[0]);
+    if(numHidden == 1){
+    	hiddenLayers[0].project(outputLayer);
+    }else if(numHidden == 2){
+    	hiddenLayers[0].project(hiddenLayers[1]);
+    	hiddenLayers[1].project(outputLayer);
+    }else{
+    	for(var index = 1; index < numHidden - 1; index++) {
+    		console.log(hiddenLayers[index],hiddenLayers[index+1]);
+			hiddenLayers[index].project(hiddenLayers[index+1]);
+		}
+   		hiddenLayers[numHidden - 1].project(outputLayer);
+    }
+    
+	// function train(){
+// 		var trainers = [];
+// 		for(var index = 0; index < numHidden; index++){
+// 		// check this method
+// 			trainers.push(new Synaptic.Trainer(hiddenLayers[index]));
+// 			//console.log(trainers[index].XOR());
+// 		}
+// 	}
+// 	new train();
     // set the layers
-    this.set({
-        input: inputLayer,
-        hidden: [hiddenLayer],
-        output: outputLayer
+    return new Synaptic.Network({
+    	input: inputLayer,
+    	hidden: hiddenLayers,
+    	output: outputLayer
     });
 }
-Perceptron.prototype = new Synaptic.Network();
-Perceptron.prototype.constructor = Perceptron;
 
+var numHidden = 2;
+var myNetwork = createNetwork(2,10,1,numHidden);
 
-
-var myPerceptron = new Synaptic.Perceptron(2,3,1);
-var myTrainer = new Trainer(myPerceptron);
-
-myTrainer.XOR(); // { error: 0.004998819355993572, iterations: 21871, time: 356 }
-
-console.log(myPerceptron.activate([0,0])); // 0.0268581547421616
-myPerceptron.activate([1,0]); // 0.9829673642853368
-myPerceptron.activate([0,1]); // 0.9831714267395621
-myPerceptron.activate([1,1]); // 0.02128894618097928
-
-function network()
-{
-	while()
-	{
-	
-	
-	}
-
-
-}
+var myTrainer = new Synaptic.Trainer(myNetwork);
+ 
+ console.log(myTrainer.XOR({
+ 	iterations: 300000,
+ 	log: true,
+ 	shuffle: true,
+ 	cost: Synaptic.Trainer.cost.MSE
+ })); // { error: 0.004998819355993572, iterations: 21871, time: 356 }
+ 
+console.log(myNetwork.activate([0,0])); // 0.0268581547421616
+myNetwork.activate([1,0]); // 0.9829673642853368
+myNetwork.activate([0,1]); // 0.9831714267395621
+myNetwork.activate([1,1]); // 0.02128894618097928
+//usrname GeKoh
