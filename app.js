a8188f67614300ff88adb793ec401e784b9062c4
Julian Meyer <julianmeyer2000@gmail.com>
2015-11-08 00:25:01 -0800

    removed csrf

--- app.js
+++ app.js
@@ -80,11 +80,6 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
-app.use(lusca({
-  csrf: true,
-  xframe: 'SAMEORIGIN',
-  xssProtection: true
-}));
 app.use(function(req, res, next) {
   res.locals.user = req.user;
   next();
